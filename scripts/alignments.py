ed233ea99bd659019b36b23806d2d1caef47e681
Pavel Yakovlev <zhmactep@gmail.com>
2016-07-21 11:51:50 +0300

    Fasta formatter added

--- scripts/alignments.py
+++ scripts/alignments.py
@@ -32,17 +32,17 @@ for x in open(file_in, "r").readlines():
     m = arr[1]
     a1, a2, a3, a4 = arr[6].split(), arr[7].split(), arr[8].split(), arr[9].split()
     s = [int(a1[0]) * 3, int(a1[1]) * 3 + 3, int(a2[0]) * 3, int(a2[1]) * 3 + 3, int(a3[0]) * 3, int(a3[1]) * 3 + 3, int(a4[0]) * 3, int(a4[1]) * 3 + 3]
-    t1.append(SeqRecord(Seq(m[s[0]:s[1]])))
-    l1.append(SeqRecord(Seq(m[s[1]:s[2]])))
-    t2.append(SeqRecord(Seq(m[s[2]:s[3]])))
-    l2.append(SeqRecord(Seq(m[s[3]:s[4]])))
-    t3.append(SeqRecord(Seq(m[s[4]:s[5]])))
-    l3.append(SeqRecord(Seq(m[s[5]:s[6]])))
-    t4.append(SeqRecord(Seq(m[s[6]:s[7]])))
+    fwr1.append(SeqRecord(id=arr[0], description="", seq=Seq(m[s[0]:s[1]])))
+    cdr1.append(SeqRecord(id=arr[0], description="", seq=Seq(m[s[1]:s[2]])))
+    fwr2.append(SeqRecord(id=arr[0], description="", seq=Seq(m[s[2]:s[3]])))
+    cdr2.append(SeqRecord(id=arr[0], description="", seq=Seq(m[s[3]:s[4]])))
+    fwr3.append(SeqRecord(id=arr[0], description="", seq=Seq(m[s[4]:s[5]])))
+    cdr3.append(SeqRecord(id=arr[0], description="", seq=Seq(m[s[5]:s[6]])))
+    fwr4.append(SeqRecord(id=arr[0], description="", seq=Seq(m[s[6]:s[7]])))
 
-SeqIO.write(l1, file_out + "1", format = "fasta")
-SeqIO.write(l2, file_out + "2", format = "fasta")
-SeqIO.write(l3, file_out + "3", format = "fasta")
+SeqIO.write(cdr1, file_out + "1", format = "fasta")
+SeqIO.write(cdr2, file_out + "2", format = "fasta")
+SeqIO.write(cdr3, file_out + "3", format = "fasta")
 
 clustalomega_cline = ClustalOmegaCommandline(infile=file_out+"1", outfile=file_out+"10", verbose=True, auto=True, force=True)
 clustalomega_cline()
---
+++ scripts/fasta_formatter.py
@@ -0,0 +1,33 @@
+import argparse
+import sys
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--in_file', help='input file [csv]')
+    parser.add_argument('--out_file', help='output file [fasta]')
+    return parser
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    if not (args.in_file and args.out_file):
+        print(parser.usage)
+        sys.exit(0)
+
+    file_in = args.in_file
+    file_out = args.out_file
+    
+    with open(file_out, "wt") as fd:
+        with open(file_in, "rt") as lines:
+            for line in lines:
+                if line.find(",") == -1:
+                    continue
+                seqid, seq, fr1, fr2, fr3, fr4, fr1i, fr2i, fr3i, fr4i, shift, rflag = line.strip().split(',')
+                if int(rflag):
+                    seq = seq[::-1].replace('A', 't').replace('T', 'a').replace('C', 'g').replace('G', 'c').upper()
+                seq = seq[int(shift):]
+                fd.write(">%s\n" % seqid)
+                fd.write("%s\n" % seq)
+
+if __name__ == "__main__":
+    main()
