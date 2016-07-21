1a0c1a6574f6da2b4c424067c59c0e6109e2d380
DebNatkh <DebNatkh@gmail.com>
2016-07-21 20:38:18 +0300

    Update maxim_alignments.py (#40)

--- scripts/maxim_alignments.py
+++ scripts/maxim_alignments.py
@@ -32,17 +32,17 @@ for x in open(file_in, "r").readlines():
     m = arr[1]
     a1, a2, a3, a4 = arr[6].split(), arr[7].split(), arr[8].split(), arr[9].split()
     s = [int(a1[0]) * 3, int(a1[1]) * 3 + 3, int(a2[0]) * 3, int(a2[1]) * 3 + 3, int(a3[0]) * 3, int(a3[1]) * 3 + 3, int(a4[0]) * 3, int(a4[1]) * 3 + 3]
-    fwr1.append(SeqRecord(id=arr[0], description="", seq=Seq(m[s[0]:s[1]])))
-    cdr1.append(SeqRecord(id=arr[0], description="", seq=Seq(m[s[1]:s[2]])))
-    fwr2.append(SeqRecord(id=arr[0], description="", seq=Seq(m[s[2]:s[3]])))
-    cdr2.append(SeqRecord(id=arr[0], description="", seq=Seq(m[s[3]:s[4]])))
-    fwr3.append(SeqRecord(id=arr[0], description="", seq=Seq(m[s[4]:s[5]])))
-    cdr3.append(SeqRecord(id=arr[0], description="", seq=Seq(m[s[5]:s[6]])))
-    fwr4.append(SeqRecord(id=arr[0], description="", seq=Seq(m[s[6]:s[7]])))
+    t1.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[0]:s[1]])))
+    l1.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[1]:s[2]])))
+    t2.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[2]:s[3]])))
+    l2.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[3]:s[4]])))
+    t3.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[4]:s[5]])))
+    l3.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[5]:s[6]])))
+    t4.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[6]:s[7]])))
 
-SeqIO.write(cdr1, file_out + "1", format = "fasta")
-SeqIO.write(cdr2, file_out + "2", format = "fasta")
-SeqIO.write(cdr3, file_out + "3", format = "fasta")
+SeqIO.write(l1, file_out + "1", format = "fasta")
+SeqIO.write(l2, file_out + "2", format = "fasta")
+SeqIO.write(l3, file_out + "3", format = "fasta")
 
 clustalomega_cline = ClustalOmegaCommandline(infile=file_out+"1", outfile=file_out+"10", verbose=True, auto=True, force=True)
 clustalomega_cline()
@@ -54,15 +54,14 @@ clustalomega_cline()
 l1 = []
 l2 = []
 l3 = []
-
-for i in SeqIO.parse(file_in+"10", format="fasta"):
+for i in SeqIO.parse(file_out+"10", format="fasta"):
     l1.append(i)
-for i in SeqIO.parse(file_in+"20", format="fasta"):
+for i in SeqIO.parse(file_out+"20", format="fasta"):
     l2.append(i)
-for i in SeqIO.parse(file_in+"30", format="fasta"):
+for i in SeqIO.parse(file_out+"30", format="fasta"):
     l3.append(i)
 out_str = []
 for i in range(len(l1)):
     out_str.append(t1[i] + l1[i] + t2[i] + l2[i] + t3[i] + l3[i] + t4[i])
 
-SeqIO.write(out_str, file_out, format="fasta")
+SeqIO.write(out_str, file_out, format="fasta")  
