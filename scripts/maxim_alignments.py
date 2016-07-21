ea008d30375e6e3537ad65883712efc2d4c627c4
DebNatkh <DebNatkh@gmail.com>
2016-07-21 20:47:22 +0300

    Update maxim_alignments.py (#42)

--- scripts/maxim_alignments.py
+++ scripts/maxim_alignments.py
@@ -32,17 +32,28 @@ for x in open(file_in, "r").readlines():
     m = arr[1]
     a1, a2, a3, a4 = arr[6].split(), arr[7].split(), arr[8].split(), arr[9].split()
     s = [int(a1[0]) * 3, int(a1[1]) * 3 + 3, int(a2[0]) * 3, int(a2[1]) * 3 + 3, int(a3[0]) * 3, int(a3[1]) * 3 + 3, int(a4[0]) * 3, int(a4[1]) * 3 + 3]
-    t1.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[0]:s[1]])))
-    l1.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[1]:s[2]])))
-    t2.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[2]:s[3]])))
-    l2.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[3]:s[4]])))
-    t3.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[4]:s[5]])))
-    l3.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[5]:s[6]])))
-    t4.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[6]:s[7]])))
+    t11 = (SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[0]:s[1]])))
+    l11 = (SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[1]:s[2]])))
+    t21 = (SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[2]:s[3]])))
+    l21 = (SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[3]:s[4]])))
+    t31 = (SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[4]:s[5]])))
+    l31 = (SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[5]:s[6]])))
+    t41 = (SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[6]:s[7]])))
+    if (len(l11) * len(l21) * len(l31) == 0):
+        continue
+    else:
+        t1.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[0]:s[1]])))
+        l1.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[1]:s[2]])))
+        t2.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[2]:s[3]])))
+        l2.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[3]:s[4]])))
+        t3.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[4]:s[5]])))
+        l3.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[5]:s[6]])))
+        t4.append(SeqRecord(id=arr[0], description=arr[0], seq=Seq(m[s[6]:s[7]])))
 
 SeqIO.write(l1, file_out + "1", format = "fasta")
 SeqIO.write(l2, file_out + "2", format = "fasta")
 SeqIO.write(l3, file_out + "3", format = "fasta")
+
 
 clustalomega_cline = ClustalOmegaCommandline(infile=file_out+"1", outfile=file_out+"10", verbose=True, auto=True, force=True)
 clustalomega_cline()
