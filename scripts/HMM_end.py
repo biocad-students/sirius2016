eb4694d7d3729ff39260b6288c0ff5c1c1644c2e
Pavel Yakovlev <zhmactep@gmail.com>
2016-07-21 18:41:40 +0300

    It seems to work, but too slow

--- scripts/HMM_end.py
+++ scripts/HMM_end.py
@@ -80,6 +80,7 @@ def main():
     profile1 = tmp.file_in_good
     in_file = tmp.file_in_bad
     file_out = tmp.file_out
+    out_dir = os.path.dirname(file_out)
 
     if not (profile1 and in_file and file_out):
         print(parser.usage())
@@ -89,117 +90,129 @@ def main():
     bads = []
     LEN = 3
     for i in SeqIO.parse(in_file, format="fasta"):
-        bads.append((i.id, i.seq))
-    
-    for l in range(bads):
-        help = open('help.fasta', 'w')
-        help.write('>' + str(bads[l][0]) + '\n')
-        help.write(bads[l][1])
+        bads.append((i.id, str(i.seq)))
+    number_of_bads = len(bads)
+
+    for bad_id, bad in bads:
+        print("Fixing %s" % bad_id)
+
+        helpname = os.path.join(out_dir, 'help.fasta')
+        help = open(helpname, 'w')
+        help.write('>' + str(bad_id) + '\n')
+        help.write(bad)
         help.close()
+        print("\tHelp file created")
 
         # Profile alignment
-        profile_alignment = os.path.join(os.path.dirname(file_out), 'good.a.fasta')
-        clustalomega_cline = ClustalOmegaCommandline(infile=help,
+        profile_alignment = os.path.join(out_dir, 'help.a.fasta')
+        clustalomega_cline = ClustalOmegaCommandline(profile2=helpname,
                                                      profile1=profile1,
                                                      outfile=profile_alignment,
                                                      verbose=True, auto=True)
         clustalomega_cline()
+        print("\tClustalOmega finished the alignment")
 
         # Read goods
         arr = []
         for i in SeqIO.parse(profile_alignment, format="fasta"):
             arr.append((i.id, str(i.seq)))
         good = list(map(lambda x: x[1], arr[:-1]))  # train
-        bad = arr[-1][1]
+        aligned_bad = arr[-1][1]
         number_of_goods = len(good)
 
         with open(file_out, "wt") as fd:
-            # Main cycle
-            for l in range(number_of_bads):
-                # for each bad from chosed bads
-                bad_id, bad = arr[len(arr) - number_of_bads + i]
-                i = 0
-                while i != len(bad):  # removing inserts
-                    flag = True
-                    for j in range(number_of_goods):
-                        if good[j][i] != '-':
-                            flag = False
-                    if bad[i] != '-' and flag:
-                        bad = bad[:i] + bad[i + 1:]
-                        for j in range(number_of_goods):
-                            good[j] = good[j][:i] + good[j][i + 1:]
-                        i -= 1
-                    i += 1
-                emit_p = dict()  # prob of transition from visible to hidden states
-                trans_p = dict()  # prob of transition from hidden to hidden states
-                start_p = dict()  # prob of starting states
-                states = []  # hidden states
-                statesset = set()  # done
-                obs = []  # visible states
-                start_p['0' + bad[:LEN]] = 1
-                last = [bad[:LEN]]
-                emit_p['0' + bad[:LEN]] = {'0' + bad[:LEN]: 1}
-                for k in range(len(bad) - LEN):
-                    s = bad[k:k + LEN]  # filling obs
-                    obs.append(str(k) + s)
-                    newlast = []
-    
-                    # filling trans_p, emit_p and states
-                    for i in range(len(last)):
-                        if bad[k + LEN] == '-':
-                            arr = []
-                            summ = 0
-                            s1 = last[i] + bad[k + LEN]
-                            s2 = str(k) + last[i]
-                            for j in range(number_of_goods):
-                                dist = hamdist(s1, good[j][k:k + LEN + 1])
-                                arr.append((good[j][k + LEN], dist))
-                                summ += dist
-                            trans_p[s2] = dict()
-                            states.append(s2)
-                            statesset.add(s2)
-    
-                            for j in range(len(arr)):
-                                s3 = str(k + 1) + last[i][1:] + arr[j][0]
-                                s5 = str(k + 1) + bad[k + 1:k + LEN + 1]
-                                p = 2 / number_of_goods - arr[j][1] / summ
-                                if s3 not in trans_p[s2]:
-                                    trans_p[s2][s3] = p
-                                    emit_p[s3] = dict()
-                                    emit_p[s3][s5] = p
-                                    cond1 = k + 1 == len(bad) - LEN
-                                    cond2 = s3 not in statesset
-                                    if cond1 and cond2:
-                                        states.append(s3)
-                                        statesset.add(s3)
-                                    if '-' not in last[i][1:] + arr[j][0]:
-                                        newlast.append(last[i][1:] + arr[j][0])
-                                else:
-                                    s4 = str(k + 1) + last[i][1:] + arr[j][0]
-                                    trans_p[s2][s4] += p
-                                    emit_p[s4][s5] += p
-                        else:
-                            s6 = str(k + 1) + last[i][1:] + bad[k + LEN]
-                            trans_p[str(k) + last[i]] = dict()
-                            trans_p[str(k) + last[i]][s6] = 1
-                            states.append(str(k) + last[i])
-                            statesset.add(str(k) + last[i])
-                            emit_p[s6] = dict()
-                            emit_p[s6][str(k + 1) + bad[k + 1:k + LEN + 1]] = 1
-                            if k + 1 == len(bad) - LEN and s6 not in statesset:
-                                states.append(s6)
-                                statesset.add(s6)
-                            if '-' not in last[i][1:] + bad[k + LEN]:
-                                newlast.append(last[i][1:] + bad[k + LEN])
-                    last = newlast
-    
-                obs.append(str(k + 1) + bad[k + 1:k + LEN + 1])
-                V, opt, max_prob = viterbi(obs, states, start_p, trans_p, emit_p)
-                ans = [opt[0][1:]]
-                for i in opt[1:]:
-                    ans.append(i[-1])
-                file_out.write('>%s\n' % bad_id)
-                file_out.write('%s\n' % ''.join(ans))
+
+            print("\tRemoving inserts started")
+            position = 0
+            bad_length = len(aligned_bad)
+            while position != bad_length:  # removing inserts
+                flag = True
+                for good_number in range(number_of_goods):
+                    if good[good_number][position] != '-':
+                        flag = False
+                if aligned_bad[position] != '-' and flag:
+                    aligned_bad = aligned_bad[:position] + aligned_bad[position + 1:]
+                    for good_number in range(number_of_goods):
+                        good[good_number] = good[good_number][:position] + good[good_number][position + 1:]
+                    position -= 1
+                position += 1
+            print("\tRemoving inserts ended")
+
+            emit_p = dict()  # prob of transition from visible to hidden states
+            trans_p = dict()  # prob of transition from hidden to hidden states
+            start_p = dict()  # prob of starting states
+            states = []  # hidden states
+            statesset = set()  # done
+            obs = []  # visible states
+            start_p['0' + aligned_bad[:LEN]] = 1
+            last = [aligned_bad[:LEN]]
+            emit_p['0' + aligned_bad[:LEN]] = {'0' + aligned_bad[:LEN]: 1}
+            print("\tMatrices construction started")
+            for k in range(bad_length - LEN):
+                s = aligned_bad[k:k + LEN]  # filling obs
+                obs.append(str(k) + s)
+                newlast = []
+
+                # filling trans_p, emit_p and states
+                for i in range(len(last)):
+                    if aligned_bad[k + LEN] == '-':
+                        arr = []
+                        summ = 0
+                        s1 = last[i] + aligned_bad[k + LEN]
+                        s2 = str(k) + last[i]
+                        for good_number in range(number_of_goods):
+                            dist = hamdist(s1, good[good_number][k:k + LEN + 1])
+                            arr.append((good[good_number][k + LEN], dist))
+                            summ += dist
+                        trans_p[s2] = dict()
+                        states.append(s2)
+                        statesset.add(s2)
+
+                        for j in range(len(arr)):
+                            s3 = str(k + 1) + last[i][1:] + arr[j][0]
+                            s5 = str(k + 1) + aligned_bad[k + 1:k + LEN + 1]
+                            p = 2 / number_of_goods - arr[j][1] / summ
+                            if s3 not in trans_p[s2]:
+                                trans_p[s2][s3] = p
+                                emit_p[s3] = dict()
+                                emit_p[s3][s5] = p
+                                cond1 = k + 1 == bad_length - LEN
+                                cond2 = s3 not in statesset
+                                if cond1 and cond2:
+                                    states.append(s3)
+                                    statesset.add(s3)
+                                if '-' not in last[i][1:] + arr[j][0]:
+                                    newlast.append(last[i][1:] + arr[j][0])
+                            else:
+                                s4 = str(k + 1) + last[i][1:] + arr[j][0]
+                                trans_p[s2][s4] += p
+                                emit_p[s4][s5] += p
+                    else:
+                        s6 = str(k + 1) + last[i][1:] + aligned_bad[k + LEN]
+                        trans_p[str(k) + last[i]] = dict()
+                        trans_p[str(k) + last[i]][s6] = 1
+                        states.append(str(k) + last[i])
+                        statesset.add(str(k) + last[i])
+                        emit_p[s6] = dict()
+                        emit_p[s6][str(k + 1) + aligned_bad[k + 1:k + LEN + 1]] = 1
+                        if k + 1 == bad_length - LEN and s6 not in statesset:
+                            states.append(s6)
+                            statesset.add(s6)
+                        if '-' not in last[i][1:] + aligned_bad[k + LEN]:
+                            newlast.append(last[i][1:] + aligned_bad[k + LEN])
+                last = newlast
+            obs.append(str(k + 1) + aligned_bad[k + 1:k + LEN + 1])
+            print("\tMatrices construction ended")
+
+            print("\tViterbi started")
+            V, opt, max_prob = viterbi(obs, states, start_p, trans_p, emit_p)
+            print("\tViterbi ended")
+            ans = [opt[0][1:]]
+            for i in opt[1:]:
+                ans.append(i[-1])
+            file_out.write('>%s\n' % bad_id)
+            file_out.write('%s\n' % ''.join(ans))
+            print("\tResults put into the file")
 
 if __name__ == "__main__":
     main()
